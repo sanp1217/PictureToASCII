8dc904eada7342230f86701ddadc4487c0786972
Pablo <pablosgrad12@gmail.com>
2022-12-16 22:59:10 -0700

    Adds function that returns a 2d array of the image's pixels

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 from PIL import Image
 
+def get_pixels(im: Image) -> list:
+    pixels = im.load()
+
+    pixels_2d = []
+    for y in range(im.height):
+        row = []
+        for x in range(im.width):
+            row.append(pixels[x,y])
+        pixels_2d.append(row)
+
+    return pixels_2d
+
+def main():
+    im = Image.open("ascii-pineapple.jpg")
+    pixels = get_pixels(im)
+
+
+if __name__ == "__main__":
+    main()
