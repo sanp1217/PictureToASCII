3bee056bb23d7c2d97bd4f1d57c0376a8c55e019
Pablo <pablosgrad12@gmail.com>
2022-12-20 16:13:42 -0700

    Adds method that gets brightness matrix from pixel matrix.

--- main.py
+++ main.py
@@ -12,9 +12,34 @@ def get_pixels(im: Image) -> list:
 
     return pixels_2d
 
+def get_brightness_matrix(pixel_matrix: list) -> list:
+    brightness_matrix = []
+
+    for y in range(len(pixel_matrix)):
+        row = []
+        for x in range(len(pixel_matrix[0])):
+            R = pixel_matrix[y][x][0]
+            G = pixel_matrix[y][x][1]
+            B = pixel_matrix[y][x][2]
+            brightness = (R + G + B) / 3
+            row.append(round(brightness))
+
+        brightness_matrix.append(row)
+
+    return brightness_matrix
 def main():
     im = Image.open("ascii-pineapple.jpg")
+
     pixels_matrix = get_pixels(im)
+    print("pixel matrix length: " + str(len(pixels_matrix)) + " " + str(len(pixels_matrix[0])))
+
+
+    brightness_matrix = get_brightness_matrix(pixels_matrix)
+
+    print("brightness length: "+ (str(len(brightness_matrix))) + " " + str(len(brightness_matrix[0])))
+
+    print(brightness_matrix)
+
 
 
 if __name__ == "__main__":
